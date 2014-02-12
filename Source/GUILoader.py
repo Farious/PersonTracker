b5db288c565dd63a707801af71de3e2ec0df90db
Fábio Reis <fabio.imr.reis@gmail.com>
2014-02-12 12:33:33 +0000

    Implementing the barebones for opengl integration.

--- Source/GUILoader.py
+++ Source/GUILoader.py
@@ -1,29 +1,50 @@
 #-*- coding: utf-8 -*-
+# Copyright {2014} {Instituto Superior Técnico - Lisboa}
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 __author__ = 'Fábio'
 import sys, pygtk, gtk, gtk.glade
+import gtk.gtkgl
+# from OpenGL.GL import *
+# from OpenGL.GLU import *
 
-class HelloWorldGTK:
+# our imports
+import ImageIO as IO
+
+class PersonTrackerGUI:
+    def gtk_main_quit(self, widget, data=None):
+        print "destroy signal occurred"
+        gtk.main_quit()
+
     def __init__(self):
-        #Set the Glade file
-        self.gladeFile = './GUI/HDA3.8.glade'
-        self.wTree = gtk.glade.XML(self.gladeFile)
+        self.gladeFile = 'HDA3.8.glade'
+        self.GUIFolder = './GUI/'
 
-        #Get the Main Window and connect the 'destroy' event
-        self.window = self.wTree.get_widget("MainFrame")
+        # Initializes the GTK builder (<3.0)
+        self.gtkBuilder = gtk.Builder()
+        self.gtkBuilder.add_from_file(self.GUIFolder + self.gladeFile)
 
-        if (self.window):
-            self.window.connect("destroy", gtk.main_quit)
+        # Initializes and defines the signals\events in the window
+        self.gtkBuilder.connect_signals(self)
 
-class  QueryRelevanceEvaluationApp:
-    def __init__(self):
-        filename = "foo.glade"
-        builder = gtk.Builder()
-        builder.add_from_file('./GUI/HDA3.8.glade')
-        builder.connect_signals(self)
-        builder.get_object('TrackerMainWindow').show_all()
-        settings = gtk.settings_get_default()
-        settings.props.gtk_button_images = True
+        # Selects which window so show
+        self.gtkBuilder.get_object('TrackerMainWindow').show_all()
+
+        self.settings = gtk.settings_get_default()
+        self.settings.props.gtk_button_images = True
+
 
 if __name__ == "__main__":
-    app = QueryRelevanceEvaluationApp()
+    app = PersonTrackerGUI()
     gtk.main()
---
+++ Source/ImageIO.py
@@ -0,0 +1,40 @@
+#-*- coding: utf-8 -*-
+# Copyright {2014} {Instituto Superior Técnico - Lisboa}
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+__author__ = 'Fábio'
+
+import cv2, numpy as np
+
+class ImageIO:
+    def __init__(self):
+        self.image = 0
+        self.imageID = ''
+
+    def loadImage(self, filename):
+        imArray = cv2.imread(filename)
+
+        if imArray != None:
+            self.image = imArray
+
+    def showImage(self):
+        if self.image != None:
+            cv2.namedWindow('test', cv2.WINDOW_NORMAL)
+            cv2.imshow('test', self.image)
+            cv2.waitKey(0)
+
+if __name__ == "__main__":
+    io = ImageIO()
+    io.loadImage('./RESOURCES/IST_0005.jpg')
+    io.showImage()
